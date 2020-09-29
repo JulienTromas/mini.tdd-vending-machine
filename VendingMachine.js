5c6c5a2cce9b59eecccf2fcc439c69fb7b214823
Julien Tromas <julien.tromas@gmail.com>
2020-09-29 19:16:13 +0900

    Wrote a dispenseItem method and integrated the switch inside the pressButton
    Co-authored-by: tlightk <tlightk@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -7,21 +7,23 @@
 class VendingMachine {
   constructor() {
     this.balance = 0;
+
     this.till = {
       10: 0,
       50: 0,
       100: 0,
       500: 0,
     };
+
     this.items = {
       juice: { name: `Apple Juice`, price: 350, count: 5 },
       coffee: { name: "Tully's", price: 250, count: 7 },
-      lemonade: { name: "Citron", price: 200, count: 7 },
-      chips: { name: "Pringles", price: 400, count: 7 },
-      coke: { name: "Coca-Cola", price: 150, count: 7 },
-      greenTea: { name: "Matcha", price: 220, count: 7 },
-      fruits: { name: "Banana", price: 50, count: 7 },
-      dashi: { name: "Nani Kore", price: 450, count: 7 },
+      lemonade: { name: "Citron", price: 200, count: 70 },
+      chips: { name: "Pringles", price: 400, count: 20 },
+      coke: { name: "Coca-Cola", price: 150, count: 1 },
+      greenTea: { name: "Matcha", price: 220, count: 30 },
+      fruits: { name: "Banana", price: 50, count: 50 },
+      dashi: { name: "Nani Kore", price: 450, count: 100 },
     };
 
     this.inventory = [
@@ -32,10 +34,21 @@ class VendingMachine {
     ];
 
     this.selectedItem = [];
+
+    this.convertToIndex = [];
   }
 
   increaseTill(coin) {
     this.till[coin]++;
+  }
+
+  dispenseItem() {
+    let letter = this.convertToIndex[0];
+    let number = this.convertToIndex[1];
+    this.inventory[letter][number].count--;
+
+    console.log(`Here is your ${this.inventory[letter][number].name}`);
+    console.log(this.inventory[letter][number].count);
   }
 
   insertCoin(coin) {
@@ -50,14 +63,49 @@ class VendingMachine {
 
   pressButton(input) {
     this.selectedItem.push(input);
+
+    switch (input) {
+      case "A":
+        this.convertToIndex.push(0);
+        break;
+      case "B":
+        this.convertToIndex.push(1);
+        break;
+      case "C":
+        this.convertToIndex.push(2);
+        break;
+      case "D":
+        this.convertToIndex.push(3);
+        break;
+      case 1:
+        this.convertToIndex.push(0);
+        break;
+      case 2:
+        this.convertToIndex.push(1);
+        break;
+      case 3:
+        this.convertToIndex.push(2);
+        break;
+      case 4:
+        this.convertToIndex.push(3);
+        break;
+
+      default:
+        console.log("Please press the buttons");
+    }
+
     if (this.selectedItem.length === 2) {
-      console.log(`${this.selectedItem[0]} + ${this.selectedItem[1]}`);
+      console.log(
+        `You selected row ${this.selectedItem[0]} and column ${this.selectedItem[1]}`
+      );
+      this.dispenseItem();
     } else {
       console.log(input);
-    }
-    return input;
-  }
 
-  // stockInventory(obj) {}
+      return input;
+    }
+
+    // stockInventory(obj) {}
+  }
 }
 module.exports = VendingMachine;
