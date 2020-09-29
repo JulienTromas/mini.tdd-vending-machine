4b25f70da20ad65cdb2869d0c2d67b195d45b7ca
Julien Tromas <julien.tromas@gmail.com>
2020-09-29 17:26:52 +0900

    The machine now accepts valid coins.
    Co-authored-by: tlightk <tlightk@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -5,9 +5,28 @@
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
 class VendingMachine {
-  constructor() {}
-  insertCoin() {
-    return 2;
+  constructor() {
+    this.balance = 0;
+    this.till = {
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    };
+    this.inventory = [];
+  }
+
+  increaseTill(coin) {
+    this.till[coin]++;
+  }
+
+  insertCoin(coin) {
+    this.increaseTill(coin);
+    this.balance += coin;
+    console.log(coin);
+    console.log(this.balance);
+    console.log(this.till);
+    return coin;
   }
 }
 module.exports = VendingMachine;
