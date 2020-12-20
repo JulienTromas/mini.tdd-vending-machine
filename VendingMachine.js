59933a7391884ba246b0c75c3ae7a004d852ea05
Julien Tromas <julien.tromas@gmail.com>
2020-12-20 14:54:48 +0900

    Removed some unnecessary comments

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,9 +1,3 @@
-// your class here
-/*
-  >>> Don't forget to use module.exports!
-  What is that? Well, glad you asked.
-  Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
-*/
 class VendingMachine {
   constructor() {
     this.balance = 0;
